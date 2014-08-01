166978b91de0778a2614517f2716621492b77b0d
Matthew Robb <matthewwrobb@gmail.com>
2014-08-01 11:06:20 -0400

    Added inline docs and a tiny fix

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ var fs           = require("fs"),
     Module       = require("es6-module-transpiler/lib/module"),
     FileResolver = transpiler.FileResolver;
 
+/**
+ * Provides an es6-module-transpiler valid file resolver and optionally 
+ * allows custom hooks for different resolution phases
+ * 
+ * @constructor
+ */
 function HookedResolver(paths, hooks) {
     FileResolver.call(this, paths || []);
 
@@ -23,6 +29,15 @@ function HookedResolver(paths, hooks) {
 
 util.inherits(HookedResolver, FileResolver);
 
+/**
+ * Resolves `importedPath` imported by the given module `fromModule` to a
+ * es6-module-transpiler Module instance
+ * 
+ * @param  {String} importedPath
+ * @param  {String} fromModule
+ * @param  {Container} container
+ * @return {Module}
+ */
 HookedResolver.prototype.resolveModule = function(importedPath, fromModule, container) {
     var load  = {};
 
@@ -39,10 +54,25 @@ HookedResolver.prototype.resolveModule = function(importedPath, fromModule, cont
     return load.module;
 };
 
+/**
+ * Resolves `importedPath` imported by the given module `fromModule` to a
+ * resolvedPath to be used in the fetch operation
+ * 
+ * @param  {String} importedPath
+ * @param  {Object} load
+ * @return {String}
+ */
 HookedResolver.prototype.locate = function(importedPath, load) {
     return this.resolvePath(importedPath, load.fromModule);
 };
 
+/**
+ * Retrieves module source from location `resolvedPath`
+ * 
+ * @param  {String} resolvedPath
+ * @param  {Object} load
+ * @return {String}
+ */
 HookedResolver.prototype.fetch = function(resolvedPath, load) {
     if(load.module = load.container.getCachedModule(resolvedPath)) {
         return false;
@@ -50,16 +80,30 @@ HookedResolver.prototype.fetch = function(resolvedPath, load) {
     return fs.readFileSync(resolvedPath);
 };
 
+/**
+ * Parses module source into an es6-module-transpiler compatible ast
+ * 
+ * @param  {String} source
+ * @param  {Object} load
+ * @return {Object}
+ */
 HookedResolver.prototype.translate = function(source, load) {
     return recast.parse(source, {
         sourceFileName : path.basename(load.resolvedPath)
     });
 };
 
+/**
+ * Creates a Module instance and assigns it the ast
+ * 
+ * @param  {Object} ast
+ * @param  {Object} load
+ * @return {Module}
+ */
 HookedResolver.prototype.instantiate = function(ast, load) {
-    var mod = new Module(load.resolvedPath, load.importedPath, load.container);
-    mod.ast = ast;
-    return mod;
+    var module = new Module(load.resolvedPath, load.importedPath, load.container);
+    module.ast = ast;
+    return module;
 };
 
 HookedResolver.Module     = Module;
